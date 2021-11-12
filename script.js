9cba87a96bde3634f84322c94c38c5f1b45e419d
Lagbalu Lateef <87532030+S-Devoe@users.noreply.github.com>
2021-11-12 22:02:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+//ScrollReveal
+
+var slideLeft = {
+    distance: '150%',
+    origin: 'left',
+    opacity: null
+   };
+
+   var slideRight = {
+    distance: '150%',
+    origin: 'right',
+    opacity: null
+   };
+
+var slideUp= {
+    distance: '150%',
+    origin: 'bottom',
+    opacity: null
+    
+   };
+
+ScrollReveal().reveal(".arrow", {delay: 500});
+ScrollReveal().reveal(".standout-text", slideRight );
+ScrollReveal().reveal(".transform-text", slideLeft);
+ScrollReveal().reveal(".client-card", slideUp, {delay: 300} );
+
+
+// Navigation
+
+const menuBtn =document.querySelector(".header #menu");
+const closeBtn = document.querySelector(".nav .cancel")
+const nav = document.querySelector(".nav");
+
+menuBtn.addEventListener("click", () =>{
+    nav.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    nav.classList.remove("active");
+});
+
+
+
+
